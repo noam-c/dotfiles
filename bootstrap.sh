1b4a5775aaf0ae2d8d4824337016c1d9095a963a
Noam Chitayat <noam.chitayat+git@gmail.com>
2017-12-10 01:40:12 -0800

    Various updates to bootstrap script
    Use symlinks for dotfiles instead of copying
    Also includes backups of existing dotfiles that will be replaced.
    Installs Homebrew.
    Installs MacVim with system override.
    Installs C++ completion for YouCompleteMe.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,42 @@
 #!/bin/sh
 
-# Copy bash defaults and git prompt
-cp ./.bash_profile ~/
-cp ./.git-prompt.sh ~/
-cp ./.git-completion.bash ~/
+mkdir -p ~/dotfiles_old
 
-# Copy git settings
-cp ./.gitconfig ~/
+backup_old() {
+	[ -e $1 ]  && cp $1 ~/dotfiles_old/
+}
 
-# VIM settings
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp ./.vimrc ~/
-vim +PluginInstall +qall
+backup_old ~/.bash_profile
+backup_old ~/.gitconfig
+backup_old ~/.tmux.conf
+backup_old ~/.vimrc
 
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install hub CLI
+brew install hub
+
+# VIM settings
+brew install macvim --with-override-system-vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Install tmux
 brew install tmux
-cp ./.tmux.conf ~/
 
-# Install Hub
-brew install hub
+SCRIPT_DIR=$(dirname "$0")
+ABS_SCRIPT_DIR=$(cd $SCRIPT_DIR; pwd)
+
+ln -Fvs $ABS_SCRIPT_DIR/.bash_profile ~/.bash_profile
+ln -Fvs $ABS_SCRIPT_DIR/.gitconfig ~/.gitconfig
+ln -Fvs $ABS_SCRIPT_DIR/.vimrc ~/.vimrc
+ln -Fvs $ABS_SCRIPT_DIR/.tmux.conf ~/.tmux.conf
+
+cp ./.git-prompt.sh ~/.git-prompt.sh
+cp ./.git-completion.bash ~/.git-completion.bash
+
+# Install VIM plugins
+vim +PluginInstall +qall
+
+pushd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --tern-completer
