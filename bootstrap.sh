438575434f28f9f3ac688a64fa0edb90278753f9
Noam Chitayat <noam.chitayat+git@gmail.com>
2018-01-20 15:38:12 -0800

    Install jq and telnet, and reorganize a bit

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,19 +11,6 @@ backup_old ~/.gitconfig
 backup_old ~/.tmux.conf
 backup_old ~/.vimrc
 
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install hub CLI
-brew install hub
-
-# VIM settings
-brew install macvim --with-override-system-vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Install tmux
-brew install tmux
-
 SCRIPT_DIR=$(dirname "$0")
 ABS_SCRIPT_DIR=$(cd $SCRIPT_DIR; pwd)
 
@@ -31,6 +18,19 @@ ln -Fvs $ABS_SCRIPT_DIR/bash_profile ~/.bash_profile
 ln -Fvs $ABS_SCRIPT_DIR/gitconfig ~/.gitconfig
 ln -Fvs $ABS_SCRIPT_DIR/vimrc ~/.vimrc
 ln -Fvs $ABS_SCRIPT_DIR/tmux.conf ~/.tmux.conf
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install helpful utilities
+brew install hub
+brew install tmux
+brew install jq
+brew install telnet
+
+# VIM settings
+brew install macvim --with-override-system-vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp ./git-prompt.sh ~/.git-prompt.sh
 cp ./git-completion.bash ~/.git-completion.bash
