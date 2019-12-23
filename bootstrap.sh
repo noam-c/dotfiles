c2542cb284d2c7201bf02f2cb6cbd7fb2aa176b5
Noam Chitayat <noam.chitayat@gmail.com>
2019-12-23 11:43:42 -0800

    Moving to fish!

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,10 +20,12 @@ mkdir ~/.ssh
 SCRIPT_DIR=$(dirname "$0")
 ABS_SCRIPT_DIR=$(cd $SCRIPT_DIR; pwd)
 
-ln -Fvs $ABS_SCRIPT_DIR/bash_profile ~/.bash_profile
-ln -Fvs $ABS_SCRIPT_DIR/gitconfig ~/.gitconfig
-ln -Fvs $ABS_SCRIPT_DIR/vimrc ~/.vimrc
-ln -Fvs $ABS_SCRIPT_DIR/tmux.conf ~/.tmux.conf
+ln -Fvs $ABS_SCRIPT_DIR/bash/bash_profile ~/.bash_profile
+ln -Fvs $ABS_SCRIPT_DIR/git/gitconfig ~/.gitconfig
+ln -Fvs $ABS_SCRIPT_DIR/git-prompt.sh ~/.git-prompt.sh
+ln -Fvs $ABS_SCRIPT_DIR/git-completion.bash ~/.git-completion.bash
+ln -Fvs $ABS_SCRIPT_DIR/vim/vimrc ~/.vimrc
+ln -Fvs $ABS_SCRIPT_DIR/tmux/tmux.conf ~/.tmux.conf
 
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -43,12 +45,23 @@ brew install telnet
 brew install tmux
 brew install unrar
 
+# Fish Setup
+mkdir -p ~/.config/fish/conf.d
+mkdir -p ~/.config/powerline-shell
+ln -Fvs $ABS_SCRIPT_DIR/fish/config.fish ~/.config/fish/config.fish
+ln -Fvs $ABS_SCRIPT_DIR/fish/functions ~/.config/fish/functions
+
+brew install fish
+pip install powerline-shell
+ln -Fvs $ABS_SCRIPT_DIR/fish/powerline-config.json ~/.config/powerline-shell/config.json
+
+# Fish by default
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
 # VIM settings
 brew install macvim --with-override-system-vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-cp ./git-prompt.sh ~/.git-prompt.sh
-cp ./git-completion.bash ~/.git-completion.bash
 
 # Install VIM plugins
 vim +PluginInstall +GoInstallBinaries +qall
---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+fundle install
+
+# Use clang instead of g++ for C/C++
+set -x CC clang
+set -x CXX clang++
+
+# More thorough ls by default
+alias ls "ls -ahlrt"
+
+# Alias hub as git
+hub alias -s | source
+
+# Set up direnv for directory-specific environment variables
+direnv hook fish | source
